384a0c0350c94693f0a1b961a16b849c29738918
ivanarena <ivan.x28x@gmail.com>
2023-02-11 14:11:13 +0100

    added localStorage

--- main.js
+++ main.js
@@ -2,6 +2,15 @@ const darkToggle = document.getElementById('dark-toggle');
 const lightToggle = document.getElementById('light-toggle');
 const body = document.getElementsByTagName('body')[0];
 
+if (localStorage.prefersDark === undefined) {
+    localStorage.setItem('prefersDark',
+        window.matchMedia("(prefers-color-scheme: dark)").matches);
+}
+
+if (localStorage.prefersDark === "true") {
+    darkModeOn();
+}
+
 function darkModeOn() {
     darkToggle.classList.add('not-displayed');
     lightToggle.classList.remove('not-displayed');
@@ -15,7 +24,6 @@ function darkModeOff() {
     console.log('darkModeOff');
     body.classList.remove('dark');
 }
-
 
 
 darkToggle.addEventListener('click', darkModeOn);
