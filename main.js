84eaa7027a0af2b2991dcc892bae66435d17b8e1
ivanarena <ivan.x28x@gmail.com>
2023-07-28 16:11:02 +0100

    finish

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+const projects = [
+    document.getElementById('uncaptchable'),
+    document.getElementById('qtchartifier'),
+];
+
+const redirectToProject = (name) => {
+    if (name.srcElement.id === 'uncaptchable') {
+        window.open('https://github.com/ivanarena/uncaptchable/tree/main', '_blank');
+    } else {
+        window.open('https://github.com/ivanarena/unipd-progetto-pao/tree/main', '_blank');
+    }
+};
+
+projects[0].addEventListener('click', (name) => redirectToProject(name));
+projects[1].addEventListener('click', (name) => redirectToProject(name));
+
 // TOTOP BUTTON HANDLER
 const toTop = document.getElementById('top-link');
 let timeout = null;
@@ -19,3 +35,4 @@ const observer = new IntersectionObserver(entries => {
 
 observer.observe(document.getElementsByClassName('main-heading')[0]);
 // ===============================================================
+
